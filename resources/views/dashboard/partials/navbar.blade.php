6038afc994aa6b0d5519c3dea937e94a87025e9d
ElianaSalazar-Ruiz <eliana.salazar@est.iudigital.edu.co>
2021-12-11 00:29:48 -0500

    Se agrega el proyecto

---
+++ resources/views/dashboard/partials/navbar.blade.php
@@ -0,0 +1,26 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#">Disabled</a>
+        </li>
+      </ul>
+      <form class="form-inline  my-2 my-log-0">
+    <input class="form-control mr-sm-2" type="search"placeholder=search aria-label=""> 
+    <button class= "btn btn-outline-sucess my-2 my-sm-0"type="submit">Search</button> 
+    </form>
+    </div>
+  </nav>
